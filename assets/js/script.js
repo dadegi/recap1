e57ec32097ec83dc04af1cc52322f5e6c097dc2e
Naviganet <dadegi@gmail.com>
2026-06-22 17:36:56 +0200

    Primo recap JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,61 @@ function createBoard() {
 		board.appendChild(newRow);
 	}
 }
+
+extract.addEventListener('click', putNumber);
+
+function putNumber() {
+	// Genero il numero casuale tra 1 e 90
+	let newNumber = Math.floor(Math.random() * 90) + 1;
+	if (numbers.includes(newNumber)) {
+		// Se il numero è già estratto, genero di nuovo
+		putNumber();
+	} else {
+		// Inserisco il numero nell'array e lo segno sul tabellone
+		numbers.push(newNumber);
+		render(newNumber);
+	}
+}
+
+function render(num) {
+	// Riabilito il button Ricomincia
+	start.removeAttribute('disabled');
+	// Stampo gli elementi statici nell'HTML
+	visual.textContent = 'Ultimo estratto';
+	number.textContent = num;
+	cron.textContent = numbers.join(', ');
+	// Coloro la casella che contiene il numero estratto
+	// Creo l'array delle caselle
+	const colList = document.querySelectorAll('#board .col-1');
+	// Ciclo l'array verificando il textContent
+	colList.forEach((col) => {
+		if (col.textContent == num) {
+			col.classList.remove('bg-secondary');
+			col.classList.add('bg-primary');
+		}
+	});
+}
+
+start.addEventListener('click', reset);
+
+function reset() {
+	// Avviso l'utente che si azzera tutto
+	if (!confirm('Sei sicuro? Il gioco ricomincerà da zero')) return;
+	// Se si clicca su Ok:
+	// Svuoto l'array
+	numbers.splice(0);
+	// Cancello l'HTML statico
+	visual.innerHTML = '&nbsp';
+	number.innerHTML = '&nbsp';
+	cron.innerHTML = '&nbsp';
+	// Ripristino la grafica del tabellone
+	const colList = document.querySelectorAll('#board .col-1');
+	colList.forEach((col) => {
+		if (col.classList.contains('bg-primary')) {
+			col.classList.remove('bg-primary');
+			col.classList.add('bg-secondary');
+		}
+	});
+	// Disabilito il button ricomincia
+	start.setAttribute('disabled', 'true');
+}
